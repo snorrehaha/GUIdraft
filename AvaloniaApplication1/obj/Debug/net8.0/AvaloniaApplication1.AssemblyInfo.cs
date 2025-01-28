b9d2965939726170e8b5f5d0658656f72b3adbb5
snorrehaha <snorrehaha@gmail.com>
2025-01-28 23:29:06 +0100

    debug files pls ignore

--- AvaloniaApplication1/obj/Debug/net8.0/AvaloniaApplication1.AssemblyInfo.cs
+++ AvaloniaApplication1/obj/Debug/net8.0/AvaloniaApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+017e22a31ea1d8193547f417699a4b154943fe02")]
 [assembly: System.Reflection.AssemblyProductAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
